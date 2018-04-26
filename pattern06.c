9acee404f49eb5dc8cf1b7ce4930c72a570eda49
kevingenghaopeng <kevingenghaopeng@gmail.com>
2018-04-26 17:49:12 +0900

    add 0.6

---
+++ pattern06.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[]){
+
+  char *fileName = argv[1]/*最初の引数を代入する*/;
+  FILE *fp = fopen(fileName,"r");/*fopenを使って読み込みファイルポインタを取得*/
+
+  int **matrix;
+  int raw, colmun;
+  int i,j;
+
+  for(i = 0; i < 1; i++){
+    fscanf(fp,"%d %d",&raw, &colmun);
+  }
+  /* Save matrix data into a 1 dimention array */
+  int data[raw * colmun];
+  int temp;
+  
+  for(i = 2; i < raw * colmun + 2; i++ ){
+     fscanf(fp,"%d",&temp);
+     data[i - 2] = temp;
+  }
+  /* Ask memory for 2 dimention array */
+  matrix = malloc(colmun * sizeof(int*));
+    
+  for(i = 0; i < colmun; i++){
+    matrix[i] = malloc(raw * sizeof(int));
+  }
+  /* Save data into 2 dimention array */
+  for(i = 0; i < colmun; i++){
+    for(j = 0; j < raw; j++){
+      matrix[i][j] = data[i * raw + j];
+      printf("%d ", matrix[i][j]);
+    }
+    printf("\n");
+  }
+  /* Release memory */
+  for(i = 0; i < colmun; i++){
+    free(matrix[i]);
+  }
+  free(matrix);
+
+  fclose(fp); 
+}
+
+
+
