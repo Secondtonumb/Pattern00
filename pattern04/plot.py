13b340694a7d47cbffd8d4880c022ea416358311
secondtonumb <kevingenghaopeng@gmail.com>
2018-09-16 16:01:02 +0800

    see what has been changed

--- pattern04/plot.py
+++ pattern04/plot.py
@@ -17,12 +17,12 @@ args = parser.parse_args()
 clu = len(data[0, :]) // args.ptn_num
 
 # for x in xrange(len(data[0, :])):
-#     plt.plot(xrange(len(data[:, 0])), data[:, x], label="column%d" % (x))
-for x in xrange(args.ptn_num):
+#     plt.plot(range(len(data[:, 0])), data[:, x], label="column%d" % (x))
+for x in range(args.ptn_num):
     plt.subplot(args.ptn_num // 3, 3, x + 1)
-    for y in xrange(clu):
+    for y in range(clu):
         plt.title("pattern%d" % x)
-        plt.plot(xrange(len(data[:, 0])), data[:, x *
+        plt.plot(range(len(data[:, 0])), data[:, x *
                                                clu + y], label="cluster %d" % (y))
         plt.ylim(0, 1)
         plt.legend(fontsize=6)
